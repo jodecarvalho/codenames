005789cf1c7317f0e61b832dd3c61f97cf5e3911
jodecarvalho <joaquim.dc1986@gmail.com>
2019-12-26 14:55:23 +0100

    type ----> string

--- codenames-model-git/src/main/java/fr/formation/model/Joueur.java
+++ codenames-model-git/src/main/java/fr/formation/model/Joueur.java
@@ -3,20 +3,19 @@ package fr.formation.model;
 public class Joueur extends Personne {
 	//Créé un joueur à partir d'une personne
 	//
-	private int id;
-	private Type couleur;
-	private Role role;
+	private String couleur;
+	private String role;
 	
-	public Type getCouleur() {
+	public String getCouleur() {
 		return couleur;
 	}
-	public void setCouleur(Type couleur) {
+	public void setCouleur(String couleur) {
 		this.couleur = couleur;
 	}
-	public Role getRole() {
+	public String getRole() {
 		return role;
 	}
-	public void setRole(Role role) {
+	public void setRole(String role) {
 		this.role = role;
 	}
 	
