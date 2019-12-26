9b02dd5c2b14a3f2ef54500948b1dfc849f53755
CamilleJULIO <camille.julio@soprasteria.com>
2019-12-26 16:11:57 +0100

    DAOCarteHibernate

--- codenames-jpa/src/main/java/fr/formation/dao/hibernate/DAOCarteHibernate.java
+++ codenames-jpa/src/main/java/fr/formation/dao/hibernate/DAOCarteHibernate.java
@@ -1,5 +1,77 @@
 package fr.formation.dao.hibernate;
 
-public class DAOCarteHibernate {
+import java.util.List;
+
+import javax.persistence.EntityTransaction;
+
+import fr.formation.dao.IDAO;
+import fr.formation.model.Carte;
+import fr.formation.model.Mot;
+
+public class DAOCarteHibernate extends DAOConnectionHibernate implements IDAO <Carte, Integer>{
+
+	@Override
+	public List<Carte> findAll() {
+		// TODO Auto-generated method stub
+		return em.createQuery("select c from Carte c", Carte.class).getResultList();
+	}
+
+	@Override
+	public Carte findById(Integer id) {
+		// TODO Auto-generated method stub
+		return em.find(Carte.class, id);
+	}
+
+	@Override
+	public Carte save(Carte entity) {
+		// TODO Auto-generated method stub
+		EntityTransaction tx = em.getTransaction();
+		Carte entityMerge = null;
+		tx.begin();
+		try {
+			if(entity.getId() == 0) {
+				em.persist(entity);
+			}
+			else {
+				entityMerge = em.merge(entity);			
+			}
+			tx.commit();
+		}catch(Exception e) {
+			e.printStackTrace();
+			tx.rollback();
+		}		
+		return entityMerge;
+	}
+
+	@Override
+	public void delete(Carte entity) {
+		// TODO Auto-generated method stub
+		try {
+			em.getTransaction().begin();
+			em.remove(em.merge(entity));
+			em.getTransaction().commit();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			em.getTransaction().rollback();
+		}
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		Carte carte = em.find(Carte.class, id);
+		try {
+			em.getTransaction().begin();
+			em.remove(em.merge(carte));
+			em.getTransaction().commit();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			em.getTransaction().rollback();
+		}
+		
+		
+	}
 
 }
