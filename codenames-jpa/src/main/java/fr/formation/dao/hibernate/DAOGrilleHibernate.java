eb4e839cc88a57e2b6be7ac25ae065919b28d7a1
CamilleJULIO <camille.julio@soprasteria.com>
2019-12-26 16:21:47 +0100

    DAOGrilleHibernate

--- codenames-jpa/src/main/java/fr/formation/dao/hibernate/DAOGrilleHibernate.java
+++ codenames-jpa/src/main/java/fr/formation/dao/hibernate/DAOGrilleHibernate.java
@@ -2,39 +2,77 @@ package fr.formation.dao.hibernate;
 
 import java.util.List;
 
-import fr.formation.dao.IDAO;
-import fr.formation.dao.sql.DAOConnectionSQL;
-import fr.formation.model.Grille;
+import javax.persistence.EntityTransaction;
 
-public class DAOGrilleHibernate extends DAOConnectionSQL implements IDAO <Grille, Integer>{
+import fr.formation.dao.IDAO;
+
+import fr.formation.model.Grille;
+import fr.formation.model.Joueur;
+import fr.formation.dao.hibernate.DAOConnectionHibernate;
+
+public class DAOGrilleHibernate extends DAOConnectionHibernate implements IDAO <Grille, Integer>{
 
 	@Override
 	public List<Grille> findAll() {
 		// TODO Auto-generated method stub
-		return null;
+		return em.createQuery("select g from Grille g", Grille.class).getResultList();
 	}
 
 	@Override
 	public Grille findById(Integer id) {
 		// TODO Auto-generated method stub
-		return null;
+		return em.find(Grille.class, id);
 	}
 
 	@Override
 	public Grille save(Grille entity) {
 		// TODO Auto-generated method stub
-		return null;
+		EntityTransaction tx = em.getTransaction();
+		Grille entityMerge = null;
+		tx.begin();
+		try {
+			if(entity.getId() == 0) {
+				em.persist(entity);
+			}
+			else {
+				entityMerge = em.merge(entity);			
+			}
+			tx.commit();
+		}catch(Exception e) {
+			e.printStackTrace();
+			tx.rollback();
+		}		
+		return entityMerge;
 	}
 
 	@Override
 	public void delete(Grille entity) {
 		// TODO Auto-generated method stub
+		try {
+			em.getTransaction().begin();
+			em.remove(em.merge(entity));
+			em.getTransaction().commit();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			em.getTransaction().rollback();
+		}
 		
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
+		Joueur joueur = em.find(Joueur.class, id);
+		try {
+			em.getTransaction().begin();
+			em.remove(em.merge(joueur));
+			em.getTransaction().commit();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			em.getTransaction().rollback();
+		}
 		
 	}
 
