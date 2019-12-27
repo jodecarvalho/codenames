4ae3079fe962771d20ca81781fd3c8218a3c1818
CamilleJULIO <camille.julio@soprasteria.com>
2019-12-27 11:52:37 +0100

    CreationPartie_v1

--- codenames-java-git/src/main/java/fr/formation/Application.java
+++ codenames-java-git/src/main/java/fr/formation/Application.java
@@ -3,13 +3,34 @@ package fr.formation;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import fr.formation.dao.IDAO;
+import fr.formation.dao.hibernate.DAOConnectionHibernate;
+import fr.formation.dao.hibernate.DAOPersonneHibernate;
+import fr.formation.model.Grille;
+import fr.formation.model.Personne;
+import fr.formation.utile.CreationPartie;
+
 public class Application {
 	
 	//public static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("NomPersistenceUnit");
-		emf.close();
+		IDAO personneHibernate = new DAOPersonneHibernate();
+		Personne personne = new Personne();
+		
+		Grille grille = new Grille();
+		CreationPartie partie = new CreationPartie();
+		
+		partie.setupGrille();
+		grille.setMesCartes(partie.getMesCartes());
+		
+		grille.getMesCartes().forEach(c -> {
+			System.out.print(c.getMonMot().getMot());System.out.print(c.getPos_x());System.out.print(c.getPos_y());
+			System.out.print(c.getCouleur());System.out.println(c.isDecouvert());
+		});
+		
+		
+		DAOConnectionHibernate.close();
 	}
 
 }
--- codenames-model-git/src/main/java/fr/formation/model/Grille.java
+++ codenames-model-git/src/main/java/fr/formation/model/Grille.java
@@ -94,32 +94,32 @@ public class Grille {
 //
 //		
 //
-		for (int i = 0; i < 25; i++) {
-			Carte maCarte = new Carte();
-			DAOMotHibernate motSQL = new DAOMotSQL();
-			maCarte.getMonMot().setIdRandom();
-			maCarte.setMonMot(motSQL.findById(maCarte.getMonMot().getId())) ;
-
-			// Vérifie si la carte existe déjà
-			boolean b = true;
-			while (b) {
-				b = false;
-				for (int j = 0; j < mesCartes.size(); j++) {
-					String motGrille = mesCartes.get(j).getMonMot().getMot();
-					String newMot = maCarte.getMonMot().getMot();
-					if (motGrille.equals(newMot)) {
-						maCarte.getMonMot().setIdRandom();
-						maCarte.setMonMot(motSQL.findById(maCarte.getMonMot().getId())) ;
-						b = true;
-					}
-				}
-			}
-			
-			
-
-			mesCartes.add(maCarte);
-
-		}
+//		for (int i = 0; i < 25; i++) {
+//			Carte maCarte = new Carte();
+//			DAOMotHibernate motSQL = new DAOMotSQL();
+//			maCarte.getMonMot().setIdRandom();
+//			maCarte.setMonMot(motSQL.findById(maCarte.getMonMot().getId())) ;
+//
+//			// Vérifie si la carte existe déjà
+//			boolean b = true;
+//			while (b) {
+//				b = false;
+//				for (int j = 0; j < mesCartes.size(); j++) {
+//					String motGrille = mesCartes.get(j).getMonMot().getMot();
+//					String newMot = maCarte.getMonMot().getMot();
+//					if (motGrille.equals(newMot)) {
+//						maCarte.getMonMot().setIdRandom();
+//						maCarte.setMonMot(motSQL.findById(maCarte.getMonMot().getId())) ;
+//						b = true;
+//					}
+//				}
+//			}
+//			
+//			
+//
+//			mesCartes.add(maCarte);
+//
+//		}
 //
 //	}
 
